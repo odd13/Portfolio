868c8852eae7993e94460c811aadeb8e52fe5a31
Oddie <arcadeoddie@gmail.com>
2017-01-29 19:55:19 +1100

    Added skills and showed them in the index page for welcome.

---
+++ app/controllers/skills_controller.rb
@@ -0,0 +1,47 @@
+class SkillsController < ApplicationController
+	def index
+		@skills = Skills.all
+	end
+	def new
+    @skill = Skill.new
+  end
+	def create
+	 @skill = Skill.new(skill_params)
+	 if @skill.save
+		 #redirect_to :action => 'index' #render 'skills/index' # Handle a successful save.
+		 redirect_to '/skills'
+	 else
+		 redirect_to '/skills/new'  #render 'skills/new'
+	 end
+ end
+ def self.show
+	 @skill = Skill.find(params[:id])
+ end
+
+ def destroy
+	 @skill = Skill.find(params[:id])
+	 @skill.destroy
+	 flash[:success] = "skill deleted"
+	 redirect_to '/skills'
+ end
+
+ def edit
+	 @skill = Skill.find(params[:id])
+ end
+
+ def update
+	 @skill = Skill.find(params[:id])
+	 if @skill.update(skill_params)
+		 @skill.save
+		 redirect_to action: :index
+	 else
+		 render 'skills/edit'
+	 end
+ end
+
+ private
+
+	 def skill_params
+		 params.require(:skill).permit(:name, :description, :loggedBy)
+	 end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+skills = Skill.create([{name: 'Ruby', is_active: true}, {name: 'Rails', is_active: true}, {name: 'Arduino', is_active: true}])
