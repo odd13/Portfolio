759fd1f2b55cc7df05b60b76ef94c1b7f04eac6b
oddie <arcadeoddie@gmail.com>
2017-01-30 21:03:58 +1100

    Added articles. This will be the Projects section of the portfolio site. Adjusted look.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-skills = Skill.create([{name: 'Ruby', is_active: true}, {name: 'Rails', is_active: true}, {name: 'Arduino', is_active: true}])
+skills = Skill.create([{name: 'HTML', is_active: true}, {name: 'Linux', is_active: true}, {name: 'CSS', is_active: true}, {name: 'Ruby on rails', is_active: true}, {name: 'Arduino', is_active: true}, {name: 'Python', is_active: true}, {name: 'C', is_active: true}, {name: 'C++', is_active: true}, {name: 'Git', is_active: true}, {name: 'Heroku', is_active: true}, {name: 'Bash scripts', is_active: true}, {name: 'MySQL', is_active: true}, {name: 'MySQL', is_active: true}, {name: 'Postgresql', is_active: true}, {name: 'Reports builder', is_active: true}, {name: 'VB', is_active: true}, {name: 'JavaScript', is_active: true}, {name: 'Java', is_active: true}, {name: 'Google cloud', is_active: true}, {name: 'VPS deployments', is_active: true}, {name: 'Meteor', is_active: true}])
