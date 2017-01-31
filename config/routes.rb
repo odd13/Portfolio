3370209ef255c79c3b916859695d452198a9a08b
oddie <arcadeoddie@gmail.com>
2017-02-01 07:33:02 +1100

    changed to views to reflect the review changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # articles are manually placed into the database via a sql query
   # migration
   # add :create or :update actions as needed
-  resources :articles, only: [:index, :get] do
+  resources :articles, only: [:index, :get, :show] do
     resources :comments, only: [:index, :get, :create]
   end
 
