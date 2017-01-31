fce35a742990ec521439b445f9d44b9757bf472f
gareth <g@betechnology.com.au>
2017-01-31 21:44:42 +1100

    cleanup formatting in seeds, make the file idn seeds, make the file idempotent and force the active flag to be true by default

--- db/migrate/20170129022439_create_skills.rb
+++ db/migrate/20170129022439_create_skills.rb
@@ -1,8 +1,11 @@
 class CreateSkills < ActiveRecord::Migration[5.0]
   def change
     create_table :skills do |t|
-			t.string :name
-			t.boolean :is_active
+      t.string :name,         null: false
+      t.boolean :is_active,   default: true, null: false
+
+      # TODO: consider a rank column, such that you can rate how competent
+      # you are out of 5.
       t.timestamps
     end
   end
