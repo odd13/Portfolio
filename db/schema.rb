0d1b4f5c4aaeef0e44307747e7bdaf960ca41173
oddie <arcadeoddie@gmail.com>
2017-01-31 19:24:57 +1100

    added version. Changed to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170130132332) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "add_thumnailurl_to_articals", force: :cascade do |t|
     t.text     "thumurl"
     t.datetime "created_at", null: false
@@ -38,7 +41,7 @@ ActiveRecord::Schema.define(version: 20170130132332) do
     t.integer  "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["article_id"], name: "index_comments_on_article_id", using: :btree
   end
 
   create_table "skills", force: :cascade do |t|
