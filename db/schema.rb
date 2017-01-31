fc7ddf10cc496dba50f9a7f40fcef7f91ffe5245
Oddie <arcadeoddie@gmail.com>
2017-01-31 21:56:15 +1100

    started adding rubocop. No changes made. Prior to Devise gem intergration

--- db/schema.rb
+++ db/schema.rb
@@ -15,18 +15,6 @@ ActiveRecord::Schema.define(version: 20170130132332) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "add_thumnailurl_to_articals", force: :cascade do |t|
-    t.text     "thumurl"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "add_thumurl_to_articles", force: :cascade do |t|
-    t.text     "thumurl"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
@@ -51,4 +39,5 @@ ActiveRecord::Schema.define(version: 20170130132332) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "articles"
 end
