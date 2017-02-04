856e79c9d7b98f3acb16e5fe7e7a74a4fdfd912d
Oddie <arcadeoddie@gmail.com>
2017-02-04 20:04:46 +1100

    added assets precompile

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Portfolio
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.assets.paths << Rails.root.join( "blog")
+    config.assets.paths << Rails.root.join( "assets", "blog")
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
